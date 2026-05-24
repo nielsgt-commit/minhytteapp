cdbb1429ff74b7917263327f93568b0a0b36cda8
nielsgt-commit <182601819+nielsgt-commit@users.noreply.github.com>
2026-05-24 11:09:15 +0200

    remove redundant tables in db

---
+++ drizzle/0054_sudden_pride.sql
@@ -0,0 +1,11 @@
+ALTER TABLE "routines" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "room_adjacencies" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "structure_adjacencies" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+DROP TABLE "routines" CASCADE;--> statement-breakpoint
+DROP TABLE "room_adjacencies" CASCADE;--> statement-breakpoint
+DROP TABLE "structure_adjacencies" CASCADE;--> statement-breakpoint
+ALTER TABLE "maintenance" DROP CONSTRAINT "maintenance_routine_position_pairing";--> statement-breakpoint
+ALTER TABLE "maintenance" DROP CONSTRAINT "maintenance_routine_id_routines_id_fk";
+--> statement-breakpoint
+ALTER TABLE "maintenance" DROP COLUMN "routine_id";--> statement-breakpoint
+ALTER TABLE "maintenance" DROP COLUMN "routine_position";
